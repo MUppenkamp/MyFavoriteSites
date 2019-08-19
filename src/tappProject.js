130d0201abb4a8e4b1ee30681026054aaafa6ce2
Mona <m.uppenkamp@gmx.de>
2019-08-19 17:20:47 +0200

    :art: Improve structure with components

--- src/tappProject.js
+++ src/tappProject.js
@@ -1,12 +1,20 @@
-import { _formular } from './utils/formular.js';
+import  formular from './components/formular.js';
+import { _intro } from './components/intro.js';
+import { _getDataFetch } from './components/fetch.js';
+
+const dataURL = 'https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50';
 
 export default class TappProject {
     constructor() {
         this._setDomElements();
-        this._getDataFetch();
+        _getDataFetch(dataURL).then((data) => this._createFavList(data.Data));
     }
 
     _setDomElements() {
+        this.$tapp = document.querySelector('.tapp');
+        _intro(this.$tapp);
+        // this.$tappContent = document.querySelector('tapp__content');
+
         this.$favList = document.querySelector('.favList');
         this.$formAcc = document.querySelector('.formAcc');
         this.$sitesAccordion = document.querySelector('.cc__list');
@@ -18,20 +26,10 @@ export default class TappProject {
         this.$formularWrapper = document.querySelector('.formularWrapper');
 
         this.$button.addEventListener('click', () => {
-            this._formular()
-            console.log('click')
-        })
-    };
-
-    _getDataFetch() {
-        fetch('https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50')
-            .then((response) => {
-                return response.json()
-            }).then((json) => {
-                this._createFavList(json.Data)
-            }).catch((err) => {
-                console.log(err)
-            });
+            console.log('click');
+            formular(this.$formularName, 
+                this.$formularEMail, this.$formularSite, this.$formularComment)
+        });
     };
 
     _createFavList(data) {
@@ -40,9 +38,15 @@ export default class TappProject {
             const logo = document.createElement('img');
             const subline = document.createElement('div');
 
+            accordionItem.setAttribute('style', 'display: flex;flex-wrap: wrap;text-align: center;justify-content: center;margin-bottom: 15px;overflow: hidden;cursor: pointer;width: 25%;');
             accordionItem.classList.add('accordion__item');
             accordionItem.classList.add('list-item--clickable');
+        //----------------------------------------Styles----------------------------------------
+            logo.setAttribute('style', 'width: 60px;height: 60px;background-size: cover;background-position: center;outline: 1px solid rgba(0,0,0,0.08);margin-right: 10px;flex-shrink: 0;');
             logo.classList.add('image');
+
+        //----------------------------------------Styles----------------------------------------
+            subline.setAttribute('style', 'text-overflow: ellipsis;white-space: nowrap;overflow: hidden;padding-top: 8px;');
             subline.classList.add('subline');
 
             accordionItem.appendChild(logo);
@@ -52,43 +56,45 @@ export default class TappProject {
             subline.innerHTML = site.appstoreName;
             accordionItem.addEventListener('click', () => chayns.openUrlInBrowser(`http://chayns.net/${site.siteId}/`));
 
+            //----------------------------------------Styles----------------------------------------
+            this.$sitesAccordion.setAttribute('style', 'display: flex;flex-wrap: wrap;text-align: center;margin-bottom: 10px;');
             this.$sitesAccordion.appendChild(accordionItem);
         })
     };
 
-    _formular() {
-        if (this.$formularName.value.length < 1 || this.$formularEMail.value.length < 1 || this.$formularSite.value.length < 1) {
-            chayns.dialog.alert('Oops', 'Bitte alle Pflichfelder ausfüllen.');
-        }
-        else if (this._emailValidation(this.$formularEMail.value) && this._urlValidation(this.$formularSite.value)) {
-            chayns.intercom.sendMessageToPage({
-                text: `Name: ${this.$formularName.value} 
-                            E-Mail: ${this.$formularEMail.value} 
-                            Site: ${this.$formularSite.value} 
-                            Kommentar: ${this.$formularComment.value}`
-            }).then((data) => {
-                if (data.status === 200)
-                    chayns.dialog.alert('Danke, ' + chayns.env.user.firstName + '!');
-            });
-        }
-        else {
-            chayns.dialog.alert('Oops', 'E-Mail oder Internetseite falsch.')
-        }
-    };
+    // _formular() {
+    //     if (this.$formularName.value.length < 1 || this.$formularEMail.value.length < 1 || this.$formularSite.value.length < 1) {
+    //         chayns.dialog.alert('Oops', 'Bitte alle Pflichfelder ausfüllen.');
+    //     }
+    //     else if (this._emailValidation(this.$formularEMail.value) && this._urlValidation(this.$formularSite.value)) {
+    //         chayns.intercom.sendMessageToPage({
+    //             text: `Name: ${this.$formularName.value} 
+    //                         E-Mail: ${this.$formularEMail.value} 
+    //                         Site: ${this.$formularSite.value} 
+    //                         Kommentar: ${this.$formularComment.value}`
+    //         }).then((data) => {
+    //             if (data.status === 200)
+    //                 chayns.dialog.alert('Danke, ' + chayns.env.user.firstName + '!');
+    //         });
+    //     }
+    //     else {
+    //         chayns.dialog.alert('Oops', 'E-Mail oder Internetseite falsch.')
+    //     }
+    // };
 
-    _emailValidation(email) {
-        var abc = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return abc.test(String(email).toLowerCase());
-    }
+    // _emailValidation(email) {
+    //     var abc = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //     return abc.test(String(email).toLowerCase());
+    // }
 
-    _urlValidation(url) {
-        var pattern = new RegExp('^(https?:\\/\\/)?' + 
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + 
-            '((\\d{1,3}\\.){3}\\d{1,3}))' + 
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + 
-            '(\\?[;&a-z\\d%_.~+=-]*)?' + 
-            '(\\#[-a-z\\d_]*)?$', 'i'); 
-        return !!pattern.test(url);
-    }
+    // _urlValidation(url) {
+    //     var pattern = new RegExp('^(https?:\\/\\/)?' +
+    //         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+    //         '((\\d{1,3}\\.){3}\\d{1,3}))' +
+    //         '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+    //         '(\\?[;&a-z\\d%_.~+=-]*)?' +
+    //         '(\\#[-a-z\\d_]*)?$', 'i');
+    //     return pattern.test(url);
+    // };
 
 }
