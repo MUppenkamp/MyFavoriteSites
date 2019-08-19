bae7e5ad8a0adbb0b66e922f942d41ab60364db3
Mona <m.uppenkamp@gmx.de>
2019-08-19 09:05:43 +0200

    :recycle:Add icons to sites

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import login from './utils/login';
 import './app.scss';
-import './css/style.scss'
+import './css/style.scss';
+import TappProject from './tappProject.js';
 
 const init = async () => {
     try {
         await chayns.ready;
         chayns.ui.initAll();
+        new TappProject();
     } catch (err) {
         console.error('No chayns environment found', err);
     }
--- src/tappProject.js
+++ src/tappProject.js
@@ -1,6 +1,7 @@
 // LINK = `//chayns.tobit.com/storage/${site.siteId}/Images/icon-72.png?_=1565958881888`
+import './css/style.scss';
 
-class TappProject {
+export default class TappProject {
     constructor() {
         this._setDomElements();
         this._getDataFetch();
@@ -8,9 +9,7 @@ class TappProject {
 
 
     _setDomElements() {
-    //     let link =`//chayns.tobit.com/storage/${linkId}/Images/icon-72.png?_=1565958881888`;
-    //      let $linkId = 
-    this.$sitesAccordion = document.querySelector('.cc__list');
+        this.$sitesAccordion = document.querySelector('.cc__list');
     };
 
     _getDataFetch() {
@@ -25,11 +24,11 @@ class TappProject {
     };
 
     _createFavList(data) {
-        for(const site of data){
-            // const itemWrapper = document.createElement('div');
+        data.forEach(site => {
+            const itemWrapper = document.createElement('div');
             const imageWrapper = document.createElement('div');
             const accordionItem = document.createElement('div');
-            const logo = document.createElement('div');
+            const logo = document.createElement('img');
             const subline = document.createElement('div');
             
             accordionItem.classList.add('accordion__item');
@@ -38,17 +37,22 @@ class TappProject {
             subline.classList.add('list-item__subtitle'); //subline
             subline.classList.add('ellipsis');
 
-            // itemWrapper.appendChild(accordionItem);
+            itemWrapper.appendChild(accordionItem);
             accordionItem.appendChild(imageWrapper);
+            imageWrapper.appendChild(logo);
             accordionItem.appendChild(subline);
 
+            try{
             logo.style.backgroundImage = `url("https://chayns.tobit.com/storage/${site.siteId}/Images/icon-72.png")`;
+            } catch(err) {
+                logo.style.backgroundImaga = css/chaynsIcon.png; }
+
             subline.innerHTML = site.appstoreName;
             accordionItem.addEventListener("click", () => chayns.openUrlInBrowser(`http://chayns.net/${site.siteId}/`));
 
             this.$sitesAccordion.appendChild(itemWrapper);
-        }
-        this.$everyItem = document.querySelectorAll('.accordion__item');
+        })
+        this.$everyItem = document.querySelectorAll('.cc__list');
     };
 
 }
