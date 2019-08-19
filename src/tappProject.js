6e54252ea7532d5e77b251287054082a78e8bcc9
Mona <m.uppenkamp@gmx.de>
2019-08-19 10:33:25 +0200

    :rewind: Discard wrong change

--- src/tappProject.js
+++ src/tappProject.js
@@ -1,6 +1,6 @@
-// LINK = `//chayns.tobit.com/storage/${site.siteId}/Images/icon-72.png?_=1565958881888`
+import './css/style.scss';
 
-class TappProject {
+export default class TappProject {
     constructor() {
         this._setDomElements();
         this._getDataFetch();
@@ -8,16 +8,51 @@ class TappProject {
 
 
     _setDomElements() {
-
+        this.$sitesAccordion = document.querySelector('.cc__list');
     };
 
     _getDataFetch() {
         fetch("https://chayns1.tobit.com/TappApi/Site/SlitteApp?SearchString=love&Skip=0&Take=50")
-            .then((response) => { 
-                return response.json() 
-            }).then((data) => { 
-                this._createList(data.Data) 
-            }) .catch((err) => { console.log(err) 
-                });
+            .then((response) => {
+                return response.json()
+            }).then((json) => {
+                this._createFavList(json.Data)
+            }).catch((err) => {
+                console.log(err)
+            });
     };
+
+    _createFavList(data) {
+        data.forEach(site => {
+            // const itemWrapper = document.createElement('div');
+            // const imageWrapper = document.createElement('div');
+            const accordionItem = document.createElement('div');
+            const logo = document.createElement('img');
+            const subline = document.createElement('div');
+            
+            accordionItem.classList.add('accordion__item');
+            accordionItem.classList.add('list-item--clickable');
+            logo.classList.add('list-item__image');
+            subline.classList.add('subline'); 
+            
+
+            // itemWrapper.appendChild(accordionItem);
+            accordionItem.appendChild(logo);
+            // imageWrapper.appendChild(logo);
+            accordionItem.appendChild(subline);
+
+            try{
+            logo.style.backgroundImage = `url('https://chayns.tobit.com/storage/${site.siteId}/Images/icon-72.png')`;
+            } catch(err) {
+                logo.style.backgroundImage = `url('css/chaynsIcon.png')`;
+            }
+
+            subline.innerHTML = site.appstoreName;
+            accordionItem.addEventListener('click', () => chayns.openUrlInBrowser(`http://chayns.net/${site.siteId}/`));
+
+            this.$sitesAccordion.appendChild(accordionItem);
+        })
+        this.$everyItem = document.querySelectorAll('.cc__list');
+    };
+
 }
