1e831e9af4e62d6cb2dd3b3268b5d28da0f91a38
Mona <m.uppenkamp@gmx.de>
2019-08-19 12:09:20 +0200

    :art: Format code

--- src/utils/formular.js
+++ src/utils/formular.js
@@ -1,27 +1,27 @@
 export function _formular() {
-        if (this.$formularName.value.length < 1 || this.$formularEMail.value.length < 1 || this.$formularSite.value.length < 1) {
-            chayns.dialog.alert('Oops', 'Bitte alle Pflichfelder ausfüllen.');
-        }
-        else if (this._emailValidation(this.$formularEMail.value)) {
-            chayns.intercom.sendMessageToPage({
-                text: `Name: ${this.$formularName.value} 
+    if (this.$formularName.value.length < 1 || this.$formularEMail.value.length < 1 || this.$formularSite.value.length < 1) {
+        chayns.dialog.alert('Oops', 'Bitte alle Pflichfelder ausfüllen.');
+    }
+    else if (this._emailValidation(this.$formularEMail.value)) {
+        chayns.intercom.sendMessageToPage({
+            text: `Name: ${this.$formularName.value} 
                         E-Mail: ${this.$formularEMail.value} 
                         Site: ${this.$formularSite.value} 
                         Kommentar: ${this.$formularComment.value}`
-            }).then((data) => {
-                if (data.status === 200)
-                    chayns.dialog.alert('Danke, ' + chayns.env.user.firstName + '!');
-            });//.then(() => {
-            //     this.$formularWrapper.classList.remove('accordion--open');
-            //     this.$sitesAccordion.classList.add('accordion--open');
-            // });
-        }
-        else {
-            chayns.dialog.alert('Oops', 'E-Mail oder Internetseite falsch.')
-        }
+        }).then((data) => {
+            if (data.status === 200)
+                chayns.dialog.alert('Danke, ' + chayns.env.user.firstName + '!');
+        });//.then(() => {
+        //     this.$formularWrapper.classList.remove('accordion--open');
+        //     this.$sitesAccordion.classList.add('accordion--open');
+        // });
     }
+    else {
+        chayns.dialog.alert('Oops', 'E-Mail oder Internetseite falsch.')
+    }
+}
 
 export function _emailValidation(email) {
-        var abc = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return abc.test(String(email).toLowerCase());
-    }
+    var abc = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return abc.test(String(email).toLowerCase());
+}
